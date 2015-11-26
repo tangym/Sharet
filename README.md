34c3d6b460ab177717319b36a27373e3ff02c96c
root <root@budgetvm.tangym.me>
2015-11-26 02:42:39 -0500

    implement file recognization and routing

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Sharet
-Web-based small file sharing.
+Share files easily. No need for database.
+
+## Usage
+TODO
+
+## Instruction
+### Configuration
+`Sharet.conf` uses json format to configure the program.
+
+### Backup File
+The program uses backup file configured in `Sharet.conf` to record file routes.
+When the program starts, it reads the file first and recovers routes in the backup file.
+
+- check whether backup file matches files in share directory 
+  - remove the records that does not in share directory
+  - when a file in share directory is recorded, check whether it has been renamed
+  - when a file in share directory is not recorded, move it to upload directory 
+- record files in upload directory and rename & move to share directory
+  - a file has the same md5 as a file in share directory: delete this file in upload directory
+
+
+
+```
+
+---------------------------------
+```
+
+
+```json
+[
+  {
+    "name": "file1.name",
+    "route": "/file1/route",
+    "md5": "9e107d9d372bb6826bd81d3542a419d6",
+  },
+  {
+    "name": "file2.name",
+    "route": "/file2/route",
+    "md5": "d41d8cd98f00b204e9800998ecf8427e",
+  },
+]
+```
+
+
+- md5-file
+- route-file
+- route-passwd
+
---
+++ Sharet.py
@@ -0,0 +1,130 @@
+# -- encoding: utf-8 --
+# author: tym
+# date: 2015-11-25
+
+import hashlib
+import random
+import json
+import os
+import sys
+import time
+
+from tinydb import TinyDB, Query
+
+
+with open('Sharet.conf') as f:
+    config = json.load(f)
+
+if not os.path.exists(config['upload_dir']):
+    os.mkdir(config['upload_dir'])
+elif not os.path.isdir(config['upload_dir']):
+    print('Error: cannot create upload directory')
+    sys.exit(1)
+if not os.path.exists(config['share_dir']):
+    os.mkdir(config['share_dir'])
+elif not os.path.isdir(config['share_dir']):
+    print('Error: cannot create share directory')
+    sys.exit(1)
+
+db = TinyDB(config['database'], indent=1)
+File = Query()
+
+def md5(fpath):
+    hash = hashlib.md5()
+    with open(fpath, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash.update(chunk)
+    return hash.digest()
+
+
+def byte_to_hex(byte_code):
+    return ''.join('{:02x}'.format(x) for x in byte_code).upper()
+
+
+def route(byte_code, length=4):
+    length = 1 if length < 1 else int(length)
+    length = len(byte_code) * 8 if length > len(byte_code) * 8 else length
+
+    step = len(byte_code) * 8 // length
+    shift = random.randint(0, len(byte_code) * 8)
+
+    def get_bits(bias):
+        bias = bias % (len(byte_code) * 8)
+        index =  (bias // 8)
+        bias -= index * 8
+        bits = 0
+
+        bits += (byte_code[index] & (0xf8 >> bias))
+        # use 5 digits
+        next_bias = 5 - (8 - bias)
+        if next_bias > 0:
+            bits = bits << next_bias
+            bits += (byte_code[(index + 1) % len(byte_code)] >> (8 - next_bias))
+        return bits
+
+    shorten_bytes = [get_bits(i * step + shift) for i in range(length)]
+
+    def bytes_to_string(shorten_bytes):
+        chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_'
+        shorten_chars = ''
+
+        for byte in shorten_bytes:
+            index = byte & 0x1f
+            shorten_chars += chars[index]
+        return shorten_chars
+
+    return bytes_to_string(shorten_bytes)
+
+
+# scan share directory.
+flist = os.listdir(config['share_dir'])
+files = {byte_to_hex(md5((os.path.join(config['share_dir'], fname)))): fname 
+         for fname in flist}
+
+# remove records of which the file does not exist in share directory
+for file in db.all():
+    if file['md5'] in files:
+        db.update({'name': files[file['md5']]}, File.md5==file['md5'])
+    else:
+        db.remove(File.md5==file['md5'])
+
+# move files in share directory to upload directory if not recorded
+for file in files:
+    if not db.contains(File.md5==file):
+        os.rename(os.path.join(config['share_dir'], files[file]),
+                  os.path.join(config['upload_dir'], files[file]))
+
+# record files in upload directory and move to share direcory
+def upload(fname=None):
+    if fname:
+        if os.path.isfile(os.path.join(config['upload_dir'], fname)):
+            file = {}
+            fmd5 = md5(os.path.join(config['upload_dir'], fname))
+            file['md5'] = byte_to_hex(fmd5)
+            file['name'] = '%s-%s' % (file['md5'][:config['prefix_length']], fname)
+            file['route'] = route(fmd5)
+            file['upload_time'] = time.strftime('%Y-%m-%d %H:%M:%S', 
+                                                time.localtime(time.time()))
+
+            retry = 0
+            while db.contains(File.route==file['route']) and retry < 5:
+                file['route'] = route(fmd5)
+                retry += 1
+            if not db.contains(File.route==file['route']):
+                db.insert(file)
+                os.rename(os.path.join(config['upload_dir'], fname),
+                          os.path.join(config['share_dir'], file['name']))
+            else:
+                print('Error: route conflict')
+                sys.exit(1)
+        else: 
+            print('Error: upload file does not exist')
+            sys.exit(1)
+    else:
+        flist = os.listdir(config['upload_dir'])
+        for fname in flist:
+            print(fname, flist)
+            upload(fname)
+
+upload()
+
